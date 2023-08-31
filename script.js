85b84be05fab28748701fd5e8cd7a2c92838dbec
FabianCode03 <behrendtf5@gmail.com>
2023-08-31 22:24:42 +0200

    Plan detailed structure of the functions

--- script.js
+++ script.js
@@ -3,18 +3,37 @@
 /*
 
 -write a function getComputerChoice() that randomly selects rock, paper
- or scissors and returns it in some form. Maybe represented by some numeric values like 1, 0 and -1. 
+ or scissors and returns it as string
+    
+    -write a math expression that randomly selects 0, 1 or 2
+    -map 0, 1 and 2 to rock, paper nad scissors and return it as string
 
--test the function in the console 
+
 
 -write a function getPlayerChoice() that prompts the user to select rock, paper 
  or scissors and returns that value in a normalized/uniform way
+ 
+    -prompt the user for his choice
+    -convert to lower case
+    -return the normalized (lower case) choice
 
--test the function in the console
 
--write a function playOneRound(playerSelection, computerSelection) that determines the winner of the 
- round and returns a string with the winner and the interaction that decided the outcome in the style of ""You Lose! Paper beats Rock"" 
 
--test the function in the console
+-write a function determineWinner(playerSelection, computerSelection) that determines the winner of the 
+ round and returns a string with the winner and the interaction that decided the outcome in the style of "You Lose! Paper beats Rock"
+    
+    -implement switch that returns the resultString depending on the playerSelection and the computerSelection
+
+
+
+- write a function playOneRound() that executes a complete round of the game with the required output
+
+    -initialize and assign the playerSelection by running getPlayerChoice()
+    -check if valid user input:
+        -if true continue
+        -if false terminate program with notification about wrong input
+    -initialize and assign the computerSelection by running getComputerChoice()
+    -initialize and assign the resultString by running determineWinner(playerSelection, computerSelection) 
+    -log the resultString to the console
 
 */
