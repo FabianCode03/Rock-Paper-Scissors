db2d973ac75f2e769322dfed1337cad02a22284e
FabianCode03 <behrendtf5@gmail.com>
2023-08-31 21:33:03 +0200

    Plan the core functions in comments

---
+++ script.js
@@ -0,0 +1,20 @@
+//planning the project with pseudocode for easier implementation later
+
+/*
+
+-write a function getComputerChoice() that randomly selects rock, paper
+ or scissors and returns it in some form. Maybe represented by some numeric values like 1, 0 and -1. 
+
+-test the function in the console 
+
+-write a function getPlayerChoice() that prompts the user to select rock, paper 
+ or scissors and returns that value in a normalized/uniform way
+
+-test the function in the console
+
+-write a function playOneRound(playerSelection, computerSelection) that determines the winner of the 
+ round and returns a string with the winner and the interaction that decided the outcome in the style of ""You Lose! Paper beats Rock"" 
+
+-test the function in the console
+
+*/
